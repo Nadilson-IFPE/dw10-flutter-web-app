175afd5c1331cbb7cf298328afa17daba4ea6ae1
Nadilson <nadilson@protonmail.com>
2023-05-10 11:00:08 -0300

    BaseLayout  implementation

--- dw10_delivery_backoffice/lib/src/modules/login/login_page.dart
+++ dw10_delivery_backoffice/lib/src/modules/login/login_page.dart
@@ -36,7 +36,7 @@ class _LoginPageState extends State<LoginPage> with Loader, Messages {
           break;
         case LoginStateStatus.success:
           hideLoader();
-          Modular.to.navigate('/');
+          Modular.to.navigate('/home');
           break;
         case LoginStateStatus.error:
           hideLoader();
