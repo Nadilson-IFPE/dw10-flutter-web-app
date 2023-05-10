1e7651411c8dbbc47e46c3919b2683a76f021846
Nadilson <nadilson@protonmail.com>
2023-05-10 11:10:58 -0300

    Base route with RouterOutlet

--- dw10_delivery_backoffice/lib/src/modules/home/home_page.dart
+++ dw10_delivery_backoffice/lib/src/modules/home/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
-import '../template/base_layout.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,37 +13,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
-    return BaseLayout(
-      body: Container(
-        // Testar se o app está obtendo o valor da variável no arquivo .env
-        // child: Text(Env.instance.get('backend_base_url')),
-        // Para testar o helper size_extensions
-        // child: Text(context.screenWidth.toString()),
-        child: Container(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Form(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Login'),
-                    ),
-                    validator: (String) => 'Erro',
+    return Container(
+      // Testar se o app está obtendo o valor da variável no arquivo .env
+      // child: Text(Env.instance.get('backend_base_url')),
+      // Para testar o helper size_extensions
+      // child: Text(context.screenWidth.toString()),
+      child: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    label: Text('Login'),
                   ),
+                  validator: (String) => 'Erro',
                 ),
               ),
-              SizedBox(
-                width: 200,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Botão'),
-                ),
-              )
-            ],
-          ),
+            ),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Botão'),
+              ),
+            )
+          ],
         ),
       ),
     );
