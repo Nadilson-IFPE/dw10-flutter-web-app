d4e291e0a7d75c1fc4f1c26ed7f58499edf1b3ae
Nadilson <nadilson@protonmail.com>
2023-05-11 06:54:53 -0300

    Payment Types request implementation

--- dw10_delivery_backoffice/lib/src/modules/payment_type/widgets/payment_type_item.dart
+++ dw10_delivery_backoffice/lib/src/modules/payment_type/widgets/payment_type_item.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
 import '../../../core/ui/styles/text_styles.dart';
+import '../../../models/payment_type_model.dart';
 
 class PaymentTypeItem extends StatelessWidget {
-  const PaymentTypeItem({super.key});
+  final PaymentTypeModel payment;
+
+  const PaymentTypeItem({
+    Key? key,
+    required this.payment,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +22,13 @@ class PaymentTypeItem extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Row(
           children: [
-            Image.asset('assets/images/icons/payment_cc_icon.png'),
+            Image.asset(
+              'assets/images/icons/payment_${payment.acronym}_icon.png',
+              errorBuilder: (context, error, stackTrace) {
+                return Image.asset(
+                    'assets/images/icons/payment_notfound_icon.png');
+              },
+            ),
             const SizedBox(
               width: 20,
             ),
@@ -33,7 +45,7 @@ class PaymentTypeItem extends StatelessWidget {
                   height: 10,
                 ),
                 Text(
-                  'Cartão de Crédito',
+                  payment.name,
                   style: context.textStyles.textTitle,
                 ),
               ],
